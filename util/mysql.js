bfc23c4bac8ba2dfe8e5ef642515dae4d1f67443
mohamedelsayed15 <mo.elsayed621654@gmail.com>
2023-04-15 00:15:50 +0200

    test-deploy

--- util/mysql.js
+++ util/mysql.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
     'me',
     '621654', {
         dialect: 'mysql',
-        host :'127.0.0.1',
+        host :'0.0.0.0',
         storage: "./session.mysql",
     }
 )
