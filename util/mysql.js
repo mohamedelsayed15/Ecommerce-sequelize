f8a86a2cef867d0b3f3d2365208c005c2d5ba8d9
mohamedelsayed15 <mo.elsayed621654@gmail.com>
2023-04-15 00:31:40 +0200

    production-database

--- util/mysql.js
+++ util/mysql.js
@@ -1,14 +1,24 @@
 const Sequelize = require('sequelize')
 
 
+// const sequelize = new Sequelize(
+//     'ecommerce',
+//     'me',
+//     '621654', {
+//         dialect: 'mysql',
+//         host :'127.0.0.1',
+//         storage: "./session.mysql",
+//     }
+// )
 const sequelize = new Sequelize(
-    'ecommerce',
-    'me',
-    '621654', {
-        dialect: 'mysql',
-        host :'0.0.0.0',
-        storage: "./session.mysql",
+    process.env.MYSQLDATABASE,
+    process.env.MYSQLUSER,
+    process.env.MYSQLPASSWORD,
+    {
+      host: process.env.MYSQLHOST,
+      port: process.env.MYSQLPORT,
+      dialect: 'mysql',
+      storage: './session.mysql'
     }
-)
-
+  );
 module.exports = sequelize
