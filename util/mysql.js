631e723dbe379c89340fd372355330231894c943
mohamedelsayed15 <mo.elsayed621654@gmail.com>
2023-05-03 04:13:23 +0300

    fixing-bugggg

--- util/mysql.js
+++ util/mysql.js
@@ -1,21 +1,21 @@
 const Sequelize = require('sequelize')
 
 
-const sequelize = new Sequelize(
-    'ecommerce',
-    'me',
-    '621654', {
-        dialect: 'mysql',
-        host :'127.0.0.1',
-        storage: "./session.mysql",
-    }
-)
 // const sequelize = new Sequelize(
-//     process.env.MYSQL_URL,
-//     {
+//     'ecommerce',
+//     'me',
+//     '621654', {
 //         dialect: 'mysql',
-//         storage: './session.mysql'
-//     });
+//         host :'127.0.0.1',
+//         storage: "./session.mysql",
+//     }
+// )
+const sequelize = new Sequelize(
+    process.env.MYSQL_URL,
+    {
+        dialect: 'mysql',
+        storage: './session.mysql'
+    });
 
     // process.env.MYSQLDATABASE,
     // process.env.MYSQLUSER,
